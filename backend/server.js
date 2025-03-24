4f00ee7a6acf35814bd5974bb1a9e130afa71849
ibrahim parlak <ibrahimparlak282@gmail.com>
2025-03-24 19:27:37 +0300

    server.js dosyası eklendi

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ import mongoose from "mongoose";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000 ;
+const PORT = process.env.PORT || 5000
 app.use(express.json());  // allows us to accept JSON in the body req.body
 
 app.get("/api/products", async (req, res) =>{
